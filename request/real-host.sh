7955aea7691c4eab25c51b2653e7e436e59f3507
sauceisgood <53635512+sauceisgood@users.noreply.github.com>
2019-08-27 12:11:23 +0100

    Update real-host.sh

--- request/real-host.sh
+++ request/real-host.sh
@@ -7,20 +7,20 @@ do
 clear
 echo -e "$barra"
 echo -e "${cor[3]} HOST EXTRACTOR ${cor[2]}[NEW-ADM]"
-echo -e "${cor[5]} Actualiza tu Extrator menu 7 y 8 antes de usar"
+echo -e "${cor[5]} Actualiza o Extrator menu 7 e 8 antes de usar"
 echo -e "$barra"
 echo -e "${cor[2]}[1] ${cor[4]}> ${cor[3]}EXTRACTOR HOST & SSL"
 echo -e "${cor[2]}[2] ${cor[4]}> ${cor[3]}GUARDAR HOSTS EXTRAIDOS"
 echo -e "${cor[2]}[3] ${cor[4]}> ${cor[3]}MOSTRAR STATUS DE WEB"
-echo -e "${cor[2]}[4] ${cor[4]}> ${cor[3]}GERAR PAYLOAD FUNCIONALES"
-echo -e "${cor[2]}[5] ${cor[4]}> ${cor[3]}VER PUERTOS WEB & HOST"
+echo -e "${cor[2]}[4] ${cor[4]}> ${cor[3]}GERAR PAYLOAD FUNCIONAL"
+echo -e "${cor[2]}[5] ${cor[4]}> ${cor[3]}VER PORTAS WEB & HOST"
 echo -e "${cor[2]}[6] ${cor[4]}> ${cor[3]}VER PROXY HOST & WEB"
-echo -e "${cor[2]}[7] ${cor[4]}> ${cor[3]}ACTUALIZAR PAQUETERIA HOST EXTRACTOR"
+echo -e "${cor[2]}[7] ${cor[4]}> ${cor[3]}ACTUALIZAR PACOTES HOST EXTRACTOR"
 echo -e "${cor[2]}[8] ${cor[4]}> ${cor[3]}ACTUALIZAR HOST EXTRACTOR"
 echo -e "${cor[2]}[9] ${cor[4]}> ${cor[3]}MANUAL DE USO HOST EXTRACTOR"
-echo -e "${cor[2]}[0] ${cor[4]}> ${cor[0]}SALIR DEL MENU"
+echo -e "${cor[2]}[0] ${cor[4]}> ${cor[0]}SAIR DO MENU"
 echo -e "$barra"
-echo -n "Escoger opcion: "
+echo -n "Escolher opção: "
 read opcion
 #lista de menu
 echo -e "\e[0m"
@@ -39,10 +39,10 @@ read foo
 ;;
 
 2)
-echo -e "\e[1;33mPegar los hosts para poder sacarle los estatus\e[0m";
-echo -e "\e[1;33mRecuerde presionar ENTER y luego CTRL + C\e[0m";
-echo -e "\e[1;33mPara Guardar y salir luego dijite adm\e[0m";
-echo -e "\e[1;33mPara volver al menu y reingresar al Extractor\e[0m";
+echo -e "\e[1;33mCopiar os hosts para poder verificar o status\e[0m";
+echo -e "\e[1;33mCorta ENTER e depois CTRL + C\e[0m";
+echo -e "\e[1;33mPara Guardar e salir, digita adm\e[0m";
+echo -e "\e[1;33mPara voltar ao menu e entrar novamente no Extractor\e[0m";
 echo -e "$barra"
 echo -e "\e[1;36mHOST: \e[0m";
 cat>lista-host.txt
@@ -67,7 +67,7 @@ read foo;
 5)
 echo -ne "\e[1;31m DOMINIO(IP/WEB): ";
 read NIO
-echo -ne "\e[1;31m PUERTOS(53,80):  ";
+echo -ne "\e[1;31m PORTAS(53,80):  ";
 read TOS
 sleep 2
 echo -e "\e[1;32m";
@@ -90,7 +90,7 @@ read foo
 ;;
 
 7)
-echo -ne "\033[1;36mActualizando paquetes Host Extractor"
+echo -ne "\033[1;36mActualizando pacotes Host Extractor"
 echo ""
 echo ""
 apt-get update -y > /dev/null 2>&1
@@ -111,7 +111,7 @@ echo ""
 echo ""
 rm -rf $HOME/hostextractor/ > /dev/null 2>&1
 rm -rf /etc/hostextractor
-wget -O $HOME/hostextractor.tgz https://raw.githubusercontent.com/sauceisgood/ADM-ULTIMATE-NEW-FREE/master/Install/hostextractor.tgz > /dev/null 2>&1
+wget -O $HOME/hostextractor.tgz https://raw.githubusercontent.com/sauceisgood/admultimate/master/Install/hostextractor.tgz > /dev/null 2>&1
 tar -xvf hostextractor.tgz > /dev/null 2>&1
 rm -rf $HOME/hostextractor.tgz > /dev/null 2>&1
 mv $HOME/hostextractor/ /etc/ > /dev/null 2>&1
@@ -123,19 +123,19 @@ read foo
 ;;
 
 9)
-echo -e "\e[1;36m OPCIONES:"
+echo -e "\e[1;36m OPÇÕES:"
 echo -e "\e[1;32m"
-echo "[1] Extrae url de sitios web como http y https"
-echo "[2] Un bloc para agregar sitios web "
-echo "[3] Muestra estatus de sitios web"
-echo "[4] Genera payload funcionales de sitios web / host"
-echo "[5] Muestra los puertos que esta abiertos en servicios web / IP/ proxy"
-echo " - Poner una coma después de cada puerto, ejemplo: 22,53,442,443,80,8080,3128"
-echo "[6] Ver proxy o informacion de un sitio web o IP"
-echo "[7] Actualizar paqueteria de host-extractor"
+echo "[1] Extrair hosts http e https"
+echo "[2] Hosts desbloqueados sitios web "
+echo "[3] Mostrar status de Host"
+echo "[4] Generar payloads funcionais web / host"
+echo "[5] Mostra portas abertas / IP/ proxy"
+echo " - Coloca uma virgula entre cada porta, exemplo: 22,53,442,443,80,8080,3128"
+echo "[6] Ver informação de porxy web ou IP"
+echo "[7] Actualizar pacotes de host-extractor"
 echo "[8] Actualizar script host-extractor"
-echo "[9] Manual de como usar el script host-extractor"
-echo "[0] Salir del menu 0 y 0 salir del menu en español"
+echo "[9] Manual de como usar o script host-extractor"
+echo "[0] Sair do menu 0 y 0 salir do menu em español"
 echo ""
 echo -e "$barra"
 echo -e "\e[1;31mEntrar para continuar" 
@@ -143,7 +143,7 @@ read foo
 ;;
 
 25)clear
-echo -e "\e[1;32mIngresando al menu secreto...";
+echo -e "\e[1;32mA Entrar no menu secreto...";
 sleep 2
 bash ._
 read foo
